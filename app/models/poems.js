e48143135266c80f2a6c87dae45e95f5b5bfd388
desertidaho <wilcox.brett@gmail.com>
2021-03-10 14:49:07 -0700

    Poems rendering, needs CSS

--- app/models/poems.js
+++ app/models/poems.js
@@ -4,43 +4,63 @@ export default class Poems {
   }
 
   poemsTemplate() {
-    let template = ""
-    for (let i = 0; i < this.poemsArray.length; i++) {
-      template += `
-      <div class="cell">
-        <button class="button" onclick="openReveal('#${this.poemsArray[i].id}')" style="background-color: black;">
-          <div class="card" style="border: none; background-color: black; color: white;">
-            <div class="card-section" style="background-image: linear-gradient(black 1px, rgb(38, 38, 38)); padding-bottom: 0px;">
-              <h2 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</h2>
-           </div>
-           <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; width: 100vw; height: 35vh; object-fit: cover;">
-          </div>
-        </button>
-      </div>
+    console.log(this.poemsArray)
+    let featured = `
+        <!-- Featured Project Row-->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+            <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/images/${this.poemsArray[0].img}" alt="" /></div>
+            <div class="col-xl-4 col-lg-5">
+                <div class="featured-text text-lg-left">
+                    <h3 class="mb-2">Featured Poem!</h3>
+                    <h4>${this.poemsArray[0].title}</h4>
+                    <p class="text-black-50 mb-0">${this.poemsArray[0].poem}</p>
+                </div>
+            </div>
+        </div>
+    `
 
-      <div class="reveal full" id="${this.poemsArray[i].id}" data-reveal style="color: black;">
-        <div class="grid-x grid-margin-x align-center" style="margin-top: 3rem;">
-          <h2 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</h2>
-        </div>
-        <div class="grid-x grid-margin-x align-center" style="margin-top: 10px;">
-          <p style="font-style: italic; margin-top: 1rem; margin-bottom: 3rem; font-size: 0.75rem;">${this.poemsArray[i].poem}</p>
-          <button class="close-button" data-close aria-label="Close modal" type="button" style="color: black;">
-          <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="grid-x grid-margin-x align-center" style="margin-top: 0rem; margin-bottom: 2rem;">
-          <audio controls preload="auto">
-            <source src="./assets/audio/${this.poemsArray[i].audio}" type="audio/wav">
-          </audio>
-        </div>
-        <div class="grid-x grid-margin-x align-center" style="font-size: 1.5rem; margin-bottom: 3rem;">
-          <a class="warning button" href="https://www.amazon.com/gp/product/1792611633" target="_blank" aria-labelledby="Amazon" style="color: Black;">Buy on Amazon</a>
-          <a class="warning button" href="https://commerce.coinbase.com/checkout/14e93c6a-bdd0-4eff-befb-8a869b2d26bf" target="_blank"  aria-labelledby="Bitcoin" style="color: black; margin-left: 3rem;">Donate Bitcoin</a>
-        </div>
+    let template = ""
+    for (let i = 1; i < this.poemsArray.length; i++) {
+      template += `
+      <!-- Picture Left Row-->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6"><img class="img-fluid" src="/assets/images/${this.poemsArray[i].img}" alt="" /></div>
+          <div class="col-lg-6">
+              <div class="bg-black text-center h-100 project">
+                  <div class="d-flex h-100">
+                      <div class="project-text w-100 my-auto text-center text-lg-left">
+                          <h4 class="text-white">${this.poemsArray[i].title}</h4>
+                          <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                          <hr class="d-none d-lg-block mb-0 ml-0" />
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
       `
+        for (let x = i + 1; x < this.poemsArray.length; x++) {
+        template += `
+        <!-- Picture Right Row-->
+        <div class="row justify-content-center no-gutters">
+            <div class="col-lg-6"><img class="img-fluid" src="/assets/images/${this.poemsArray[x].img}" alt="" /></div>
+            <div class="col-lg-6 order-lg-first">
+                <div class="bg-black text-center h-100 project">
+                    <div class="d-flex h-100">
+                        <div class="project-text w-100 my-auto text-center text-lg-right">
+                            <h4 class="text-white">${this.poemsArray[x].title}</h4>
+                            <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                            <hr class="d-none d-lg-block mb-0 mr-0" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        break
+      }
+      i += 1
     }
-    return template
+    return featured + template
   }
 
 }
